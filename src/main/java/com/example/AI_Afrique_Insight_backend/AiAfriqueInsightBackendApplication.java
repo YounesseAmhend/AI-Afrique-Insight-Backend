a8338b4e9ce26ec7af70421118b1aa67981bded5
youssefCOD <youssefamhendpro@gmail.com>
2025-03-14 19:29:14 +0000

    first

---
+++ src/main/java/com/example/AI_Afrique_Insight_backend/AiAfriqueInsightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AI_Afrique_Insight_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAfriqueInsightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAfriqueInsightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AI_Afrique_Insight_backend/AiAfriqueInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_Afrique_Insight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAfriqueInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
