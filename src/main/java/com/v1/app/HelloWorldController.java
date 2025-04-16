b76e9017163750bdb06ddf8468604848c042fb0e
unknown <younesseamhend2019@GMAIL.COM>
2025-04-16 13:58:32 +0100

    Added grpc dependencies

--- src/main/java/com/v1/app/HelloWorldController.java
+++ src/main/java/com/v1/app/HelloWorldController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        String hello = "Hello World and welcome to this amazing application that provides insightful AI solutions for Africa's development challenges and opportunities";
-        return hello;
-    }
+  @GetMapping("/hello")
+  public String sayHello() {
+    String hello = "Hello World and welcome to this amazing application that provides insightful AI solutions for Africa's development challenges and opportunities";
+    return hello;
+  }
 }
