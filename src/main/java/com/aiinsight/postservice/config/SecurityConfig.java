4a1d862ff05af96c7d96b90e4b12744730491dff
unknown <younesseamhend2019@GMAIL.COM>
2025-12-02 07:28:07 +0100

    changed strategy

--- src/main/java/com/aiinsight/postservice/config/SecurityConfig.java
+++ src/main/java/com/aiinsight/postservice/config/SecurityConfig.java
@@ -1,24 +1,20 @@
 package com.aiinsight.postservice.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -29,34 +25,23 @@ public class SecurityConfig {
         .cors(withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/admin/**").authenticated()
-            .anyRequest().permitAll()
-        )
+            .anyRequest().permitAll())
         .httpBasic(withDefaults())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
-  @Bean
-  public UserDetailsService userDetailsService() {
-    UserDetails admin = User.withDefaultPasswordEncoder()
-        .username("admin")
-        .password("password") 
-        .roles("ADMIN")
-        .build();
-    return new InMemoryUserDetailsManager(admin);
-  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:3001", "http://77.37.124.70:3013", "http://srv844192.hstgr.cloud:3013"));
+    configuration.setAllowedOrigins(
+        List.of("http://localhost:3001", "http://77.37.124.70:3013", "http://srv844192.hstgr.cloud:3013"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
   }
-
 
 }
